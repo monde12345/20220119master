0f65676be0d331e8698c80107345eab2f667cdca
msdm3328@naver.com <msdm3328@naver.com>
2022-01-19 18:04:57 +0900

    20220219master

---
+++ src/실습문제2회/JuminNumProcess.java
@@ -0,0 +1,25 @@
+package 실습문제2회;
+
+import java.util.Scanner;
+
+public class JuminNumProcess {
+//키보드로 입력받은 주민등록번호에서 문자열을 하이픈('-')으로 분리하여 주민번호 앞자리 6자리만(즉 생년월일) 출력,
+	//뒤  7자리만 출력하는 프로그램을 작성하세요.
+	public static void main(String[] args) {
+		String juminNum;
+		int sum = 0;	
+		int temp, result;
+		int [] weight = {2,3,4,5,6,7,0,8,9,2,3,4,5};
+		
+		System.out.print("주민번호 입력: ");
+		Scanner scan =new Scanner(System.in);
+		juminNum = scan.nextLine();
+		
+		int[] str = {2,3,4,5,6,7,0,8,9,2,3,4,5};
+		System.out.println();
+	
+	
+	
+	}
+
+}
---
+++ src/실습문제2회/ThreeSixNineGame.java
@@ -0,0 +1,10 @@
+package 실습문제2회;
+
+public class ThreeSixNineGame {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
