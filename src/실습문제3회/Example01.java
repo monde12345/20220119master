866da12e19e608e4d39a67ef36a04df75316528f
msdm3328@naver.com <msdm3328@naver.com>
2022-01-27 18:43:50 +0900

    0127EX

---
+++ src/실습문제3회/Example01.java
@@ -0,0 +1,20 @@
+package 실습문제3회;
+
+class MyClass {
+	int i;
+	private String a;
+	
+}	
+
+public class Example01 {
+	public static void main(String[] args) {
+	try {
+		MyClass a = null;	
+		a.i = 10; 				
+		
+		System.out.println(a.i);
+	}catch
+		
+	}
+	
+}
---
+++ src/실습문제3회/Example02.java
@@ -0,0 +1,10 @@
+package 실습문제3회;
+
+public class Example02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
